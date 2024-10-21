5f5540ef0d90c0166a46a68f0fb9890244458ab0
trkhduy <trkhduy003@gmail.com>
2024-10-22 06:23:41 +0700

    Test kafka local

--- identity-service/src/main/java/com/dev/identity/configuration/SecurityConfig.java
+++ identity-service/src/main/java/com/dev/identity/configuration/SecurityConfig.java
@@ -6,7 +6,6 @@ import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -32,7 +31,8 @@ public class SecurityConfig {
             "/api/v1/auth/login",
             "/api/v1/auth/introspect",
             "/api/v1/auth/logout",
-            "/api/v1/auth/refreshToken"
+            "/api/v1/auth/refreshToken",
+            "/api/v1/auth/verifyAccount"
     };
 
     @Bean
@@ -40,7 +40,7 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request ->
                         request
-                                .requestMatchers(HttpMethod.POST, AUTH_WHITELIST).permitAll()
+                                .requestMatchers(AUTH_WHITELIST).permitAll()
                                 .anyRequest().authenticated());
 
         http.oauth2ResourceServer(oauth2 ->
